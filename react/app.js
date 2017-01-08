da25aae32f074a8a22d9a2f0cc01ebb234f39ae9
Jong Hoon Kim <infoset@gmail.com>
2017-01-08 16:15:34 -0500

    init

---
+++ react/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TreeTable from './treeTable'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return  <div>
+                    <TreeTable />
+                </div>
+    }
+}
+
+export default App
